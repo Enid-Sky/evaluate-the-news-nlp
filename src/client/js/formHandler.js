772a9bc70c9f1d6e78e3cf01199d22870e368f83
Enid-Sky <enid.sky@protonmail.com>
2020-06-17 19:03:58 -0700

    tests API function with a test API

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,10 +6,12 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=94070,US&units=imperial&APPID=e131d5ea093dbc7fc2da1a0496c042c8')
         .then(res => res.json())
         .then(function (res) {
-            document.getElementById('results').innerHTML = res.message
+            document.getElementById('results').innerHTML = `The weather now is ${res.main.temp} and it looks like ${res.weather[0].description}`
+
+            console.log(`::: API DATA::: ${res}`)
         })
 }
 
