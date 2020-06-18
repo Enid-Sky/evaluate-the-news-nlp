9a1d45cc631c41347c2bdee3ab1c774636db5a10
Enid-Sky <enid.sky@protonmail.com>
2020-06-18 15:19:35 -0700

    enforces alert if field is empty

--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -1,7 +1,7 @@
 function checkForUrl(inputText) {
     console.log("::: Running checkForUrl :::", inputText);
 
-    if (inputText == '' || inputText == null) {
+    if (inputText === '' || inputText == null) {
         alert("Please enter a valid URL")
     } else {
         return true;
