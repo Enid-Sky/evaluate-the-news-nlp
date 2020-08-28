c5755e7f6e9dcceff963310a6ac288519dd3a749
Enid-Sky <enid.sky@protonmail.com>
2020-08-28 12:50:53 -0700

    require image files

--- src/client/index.js
+++ src/client/index.js
@@ -10,6 +10,10 @@ import './styles/base.scss'
 import './styles/form.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
+import './images/negative.jpg'
+import './images/neutral.jpg'
+import './images/positive.jpg'
+
 
 console.log(checkForUrl);
 
