ef4b3af6c41efa94f9f8d2453985730e0b969602
Enid-Sky <enid.sky@protonmail.com>
2020-09-09 13:40:12 -0700

    remove unnecessary commented text

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -24,6 +24,7 @@ function handleSubmit(event) {
             console.log("::: Updating UI :::")
             if(res.polarity === 'neutral') {
                 document.getElementById('polarityImg').innerHTML = "<img src=\"https://images.unsplash.com/photo-1575045113838-6bdaf17765d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80\" width=\"330px\">";
+                
             }
             if(res.polarity === 'positive'){
                 document.getElementById('polarityImg').innerHTML = "<img src=\"https://images.unsplash.com/photo-1545328951-d483a3667ed0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80\" width=\"330px\">";
@@ -31,21 +32,14 @@ function handleSubmit(event) {
             if(res.polarity === 'negative'){
                 document.getElementById('polarityImg').innerHTML = "<img src=\"https://images.unsplash.com/photo-1541848156497-67cadcfbc7de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2106&q=80\" width=\"330px\">";
             }
-            
-            
-           // let txt = document.createElement('txt');
-           // txt.innerHTML = 'Article Excerpt:'
 
-
-            //document.getElementById('excerpt').appendChild(txt)
-            //excerpt.style.fontSize = '16px';
-
-            document.getElementById('text').innerText = res.text;
             document.getElementById('subjectivity').innerText = `The nature of this article is ${res.subjectivity}.`
             subjectivity.style.fontSize = '18px';
     
             document.getElementById('polarity').innerText = `The tone of this article is ${res.polarity}.`
             polarity.style.fontSize = '18px';
+            document.getElementById('text').innerText = res.text;
+
             document.getElementById('url').value = ''
         })
 
