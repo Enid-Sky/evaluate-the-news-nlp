d05cafb02390f40f024425a5cc2da79f1b5c9cfa
Enid-Sky <enid.sky@protonmail.com>
2020-09-21 10:01:15 -0700

    add new images

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -23,13 +23,13 @@ function handleSubmit(event) {
         .then(function (res) {
             console.log("::: Updating UI :::")
             if(res.polarity === 'neutral') {
-                document.getElementById('polarityImg').innerHTML = "<img src=\"https://images.unsplash.com/photo-1575045113838-6bdaf17765d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80\" width=\"320px\">";
+                document.getElementById('polarityImg').innerHTML = "<img src=\"https://i.ibb.co/mJc1WVV/neutraldog.png\" width=\"320px\">";
             }
             if(res.polarity === 'positive'){
-                document.getElementById('polarityImg').innerHTML = "<img src=\"https://images.unsplash.com/photo-1545328951-d483a3667ed0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80\" width=\"320px\">";
+                document.getElementById('polarityImg').innerHTML = "<img src=\"https://i.ibb.co/YdrwMBL/happydog.png\" width=\"320px\">";
             }
             if(res.polarity === 'negative'){
-                document.getElementById('polarityImg').innerHTML = "<img src=\"https://images.unsplash.com/photo-1541848156497-67cadcfbc7de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2106&q=80\" width=\"320px\">";
+                document.getElementById('polarityImg').innerHTML = "<img src=\"https://i.ibb.co/BqPDf0P/baddog.png\" width=\"320px\">";
             }
 
             document.getElementById('subjectivity').innerText = `The nature of this article is ${res.subjectivity}.`
